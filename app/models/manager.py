986c320db0e8cae6cecef36f3003312204e278bc
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-06-12 11:05:34 +0530

    Update manager.py

--- app/models/manager.py
+++ app/models/manager.py
@@ -1,32 +1,3 @@
-# # from sqlalchemy import Column, Integer, String
-# # from sqlalchemy.orm import relationship
-# # from app.database.session import Base
-
-# # class Manager(Base):
-# #     __tablename__ = "managers"
-
-# #     id = Column(Integer, primary_key=True, index=True)
-# #     name = Column(String)
-# #     email = Column(String, unique=True, index=True)
-
-# #     employee_managers = relationship("EmployeeManager", back_populates="manager")
-# #     report_logs = relationship("ReportLog", back_populates="manager", cascade="all, delete-orphan")
-
-
-# from sqlalchemy import Column, Integer, String
-# from sqlalchemy.orm import relationship
-# from app.database.session import Base
-
-# class Manager(Base):
-#     __tablename__ = "managers"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String)
-#     email = Column(String, unique=True, index=True)
-
-#     employee_managers = relationship("EmployeeManager", back_populates="manager")
-#     report_logs = relationship("ReportLog", back_populates="manager")
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database.session import Base
