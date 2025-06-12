ebb89f86c20732de93570c4e489a13c0ad996f69
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-06-12 11:04:56 +0530

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,25 +1,9 @@
-# # from .employee import Employee
-# # from .manager import Manager
-# # from .report import Report
-# # from .insight import Insight
-# # from .email_log import EmailLog
-# # from .report_insight_map import ReportInsightMap
-# # from .employee_manager import EmployeeManager
-# # from .admin_user import AdminUser
-# # from app.models import employee, manager, employee_manager, report_log, email_log
-# # app/models/__init__.py
-
-# from app.models import manager
-# from app.models import employee
-# from app.models import employee_manager
-# from app.models import report_log
-# from app.models import email_log
 
 
 from .employee import Employee
 from .manager import Manager
 from .employee_manager import EmployeeManager
-from .report import Report  # ✅ Make sure this line is added
+from .report import Report 
 from .report_log import ReportLog
 from .email_log import EmailLog
 from .insight import Insight
